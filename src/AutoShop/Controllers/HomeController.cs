1204060688b97a78ec019b1f3578cef30def40fd
Azuay <dunda.deividas@gmail.com>
2017-04-09 14:10:43 +0300

    Add car sub-categories to navigation when manufacturer is selected

--- src/AutoShop/Controllers/HomeController.cs
+++ src/AutoShop/Controllers/HomeController.cs
@@ -22,10 +22,22 @@ namespace AutoShop.Controllers
                         .ToList();
                 } else
                 {
-                    cars = db.Car
-                        .Where(b => b.CarManufacturer.Equals(filter))
-                        .OrderByDescending(b => b.CarYear)
-                        .ToList();
+                    string[] filters = filter.Split('-');
+                    ViewData["Filter"] = filters[0];
+                    if (filters.Length > 1)  // Car filter exists
+                    {
+                        cars = db.Car
+                            .Where(b => b.CarManufacturer.Equals(filters[0]) && b.CarName.Equals(filters[1]))
+                            .OrderByDescending(b => b.CarYear)
+                            .ToList();
+                    }
+                    else                     // Not filtered by car
+                    {               
+                        cars = db.Car
+                            .Where(b => b.CarManufacturer.Equals(filters[0]))
+                            .OrderByDescending(b => b.CarYear)
+                            .ToList();
+                    }
                 }
                 return View(cars);
             }
